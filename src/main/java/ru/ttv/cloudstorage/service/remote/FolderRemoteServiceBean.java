ab76b325636e357b3bc85c1aaa9c15986d685e20
timteplych <timteplych@mail.ru>
2018-10-04 22:28:29 +0800

    files and folders processing classes

---
+++ src/main/java/ru/ttv/cloudstorage/service/remote/FolderRemoteServiceBean.java
@@ -0,0 +1,83 @@
+package ru.ttv.cloudstorage.service.remote;
+
+import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ru.ttv.cloudstorage.api.remote.FolderRemoteService;
+import ru.ttv.cloudstorage.api.system.ApplicationService;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.nodetype.NodeType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Timofey Teplykh
+ */
+@ApplicationScoped
+public class FolderRemoteServiceBean implements FolderRemoteService {
+
+    @Inject
+    private ApplicationService applicationService;
+
+    @Override
+    @NotNull
+    public List<String> getListFolderNameRoot() {
+        final List<String> result = new ArrayList<>();
+        try {
+            final Node root = applicationService.getRootNode();
+            final NodeIterator nt = root.getNodes();
+            while (nt.hasNext()){
+                final Node node = nt.nextNode();
+                final NodeType nodeType = node.getPrimaryNodeType();
+                final boolean isFolder = nodeType.isNodeType("nt:folder");
+                if(isFolder) result.add(node.getName());
+            }
+            return result;
+        }catch (Exception e){
+            e.printStackTrace();
+            return result;
+        }
+    }
+
+    @Override
+    @SneakyThrows
+    public void createFolder(String folderName) {
+        @Nullable final Node root = applicationService.getRootNode();
+        if(root == null) return;
+        root.addNode(folderName,"nt:folder");
+        applicationService.save();
+    }
+
+    @Override
+    public void printListFolderNameRoot() {
+        for (final String name: getListFolderNameRoot()){
+            System.out.println(name);
+        }
+    }
+
+    @Override
+    @SneakyThrows
+    public void clearRoot() {
+        final Node root = applicationService.getRootNode();
+        final NodeIterator nt = root.getNodes();
+        while (nt.hasNext()){
+            final Node node = nt.nextNode();
+            final NodeType nodeType = node.getPrimaryNodeType();
+            final boolean isFolder = nodeType.isNodeType("nt:folder");
+            if(isFolder) node.remove();
+        }
+    }
+
+    @Override
+    @SneakyThrows
+    public void removeFolder(@Nullable final String folderName) {
+        if(folderName == null || folderName.isEmpty())return;
+        final Node root = applicationService.getRootNode();
+        final Node node = root.getNode(folderName);
+        node.remove();
+    }
+}
